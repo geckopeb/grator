22fd629242f0eada74e244294b191b6a184cb99c
Pablo E. Barazzutti <geckopeb@gmail.com>
2013-09-20 20:17:27 -0300

    removed application.conf (and provided a sample)

---
+++ project/Build.scala
@@ -0,0 +1,20 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "Grator"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    "mysql" % "mysql-connector-java" % "5.1.18"
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+   ).dependsOn(RootProject( uri("git://github.com/freekh/play-slick.git") ))
+}
